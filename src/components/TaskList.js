6edfece96b0cbba1410c637efa26283d8d218870
Atul Raj <119055533+atul3121@users.noreply.github.com>
2026-01-09 21:15:34 +0530

    Delete src directory

--- src/components/TaskList.js
+++
@@ -1,7 +0,0 @@
-import TaskItem from "./TaskItem";
-
-export default function TaskList({ tasks, refresh }) {
-  return tasks.map(task => (
-    <TaskItem key={task.id} task={task} refresh={refresh} />
-  ));
-}
